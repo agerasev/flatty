215e4cec1a4d72aa1975dda103568b8e30524b6c
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-08-27 20:59:50 +0700

    Import guards to root and writer traits to prelude

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -2,11 +2,11 @@ pub mod reader;
 pub mod writer;
 
 #[cfg(feature = "async")]
-pub use reader::AsyncSharedReader;
-pub use reader::{BlockingSharedReader, ReadError, Reader};
+pub use reader::{AsyncSharedReadGuard, AsyncSharedReader};
+pub use reader::{BlockingSharedReadGuard, BlockingSharedReader, ReadError, ReadGuard, Reader};
 #[cfg(feature = "async")]
 pub use writer::AsyncSharedWriter;
-pub use writer::{BlockingSharedWriter, WriteError, Writer};
+pub use writer::{BlockingSharedWriter, UninitWriteGuard, WriteError, WriteGuard, Writer};
 
 pub mod prelude {
     #[cfg(feature = "async")]
@@ -14,7 +14,10 @@ pub mod prelude {
     pub use super::reader::BlockingReader;
     #[cfg(feature = "async")]
     pub use super::writer::AsyncWriteGuard;
+    #[cfg(feature = "async")]
+    pub use super::writer::AsyncWriter;
     pub use super::writer::BlockingWriteGuard;
+    pub use super::writer::BlockingWriter;
 }
 
 #[cfg(test)]
