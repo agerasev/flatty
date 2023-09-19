157e41a9280d44b4dd65bbbb3103dad641a2d3bd
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-09-19 09:35:24 +0700

    IO: Fixed imports

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -8,13 +8,17 @@ pub use common::*;
 
 #[cfg(feature = "blocking")]
 pub mod blocking;
+#[cfg(all(feature = "blocking", feature = "io"))]
+pub use blocking::{IoReceiver, IoSender};
 #[cfg(feature = "blocking")]
-pub use blocking::{IoReceiver, IoSender, Receiver, Sender};
+pub use blocking::{Receiver, Sender};
 
 #[cfg(feature = "async")]
 pub mod async_;
+#[cfg(all(feature = "async", feature = "io"))]
+pub use async_::{IoReceiver as AsyncIoReceiver, IoSender as AsyncIoSender};
 #[cfg(feature = "async")]
-pub use async_::{IoReceiver as AsyncIoReceiver, IoSender as AsyncIoSender, Receiver as AsyncReceiver, Sender as AsyncSender};
+pub use async_::{Receiver as AsyncReceiver, Sender as AsyncSender};
 
 #[cfg(all(test, feature = "io"))]
 mod tests;
